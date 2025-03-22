9be487a1911cbdbb2d0703b7058ed29f9e6fa825
Your Name <you@example.com>
2025-03-22 01:03:11 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -46,7 +46,7 @@ List* crea_lista() {
    for(int i = 1 ; i <=10 ; i++)
       {
         int * elemento = (int*)malloc(sizeof(int));
-         elemento = i;
+         * elemento = i;
          pushFront(L, elemento);
       }
       
